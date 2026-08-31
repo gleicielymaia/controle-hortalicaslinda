c898ec89a828fede13a02f37ccc9b8623821999e
Ana Cristina Ferreira Almeida da Silva <2025105070071@aluno.ifro.edu.br>
2026-08-31 09:28:34 -0400

    feat: adiciona listagem das colheitas

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Lista para armazenar as colheitas cadastradas
+colheitas = []
+
 
 @app.route("/")
 def inicio():
@@ -15,21 +18,23 @@ def cadastrar_colheita():
     quantidade = request.form["quantidade"]
     data = request.form["data"]
 
+    # Adiciona a colheita à lista
+    colheitas.append({
+        "hortalica": hortalica,
+        "quantidade": quantidade,
+        "data": data
+    })
+
     print("Nova colheita cadastrada:")
     print("Hortaliça:", hortalica)
     print("Quantidade:", quantidade)
     print("Data:", data)
 
-    return f"""
-        <h1>Colheita cadastrada com sucesso!</h1>
-
-        <p>Hortaliça: {hortalica}</p>
-        <p>Quantidade: {quantidade}</p>
-        <p>Data da colheita: {data}</p>
-
-        <a href="/">Voltar para o cadastro</a>
-    """
+    return render_template(
+        "colheitas.html",
+        colheitas=colheitas
+    )
 
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
